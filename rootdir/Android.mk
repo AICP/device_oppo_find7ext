c2000895663599118963fb7e7efb239bea399bfb
Jaime Lopez <jaime.lopez@gmail.com>
2014-12-26 10:38:14 +0100

    Find7ext: Storage is different on each variant
    Change-Id: I11edcc5f220a11cb41b8d40b20ec27e3c40834de

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(PRODUCT_NAME),aicp_find7ext)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -11,3 +12,13 @@ LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+endif
